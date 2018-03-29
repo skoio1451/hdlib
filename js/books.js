db4b5d9bb5b482f3812172593f1f735ef14bc40f
Sin Sungho <skoio1451@naver.com>
2018-03-29 20:58:11 +0900

    최종

--- js/books.js
+++ js/books.js
@@ -22,4 +22,63 @@ $(function(){
 
 
     });
+
+
+
+  
+  });
+  
+  //앞부분
+
+/* 
+//BOOK OF MONTH 부분 시작
+*/
+$(function(){
+
+  //초기설정
+
+  $(".monthList").css("width", 700*$('.listColl').size()+"px");
+  //.monthList 의 너비는 .listColl*700 이므로 해당 설정
+
+  $(".listColl:last").prependTo(".monthList");
+  $(".monthList").css("margin-left","-700px");
+  
+  //Left Control
+  $(".monthGallLeft").on("click",function(){
+    $(".monthGallLeft,.monthGallRight").hide();
+    //애니메이션을 위해 숨긴다
+    $(".monthList").animate({
+        marginLeft : parseInt($(".monthList")
+        .css("margin-left"))+700+"px"
+        },"slow","swing",
+        //속성은 무조건 ""안에 넣을것 
+        function(){
+        $(".monthList").css("margin-left","-700px");
+        $(".monthList .listColl:last").prependTo(".monthList");
+        $(".monthGallLeft,.monthGallRight").show();
+        
+      });
+      
+    });
+
+  //Right Control
+  $(".monthGallRight").on("click",function(){
+
+    $(".monthGallRight,.monthGallLeft").hide();
+    $(".monthList").animate({
+      marginLeft : parseInt($(".monthList")
+      .css("margin-Left"))+-700+"px"
+    },"slow","swing",
+    function(){
+
+      $(".monthList").css("margin-left","-700px");
+      $(".listColl:first").appendTo(".monthList");
+      $(".monthGallLeft,.monthGallRight").show();
+
+
+    }
+    );
+
+  });
+    
 });
